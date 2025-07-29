0235dd8fdaf0694dfa1971fac1f2a229d998152a
fadhluibnu <f.ibnu.a1@gmail.com>
2025-07-29 08:26:06 +0700

    fix: public storage

--- start.bat
+++ start.bat
@@ -25,13 +25,19 @@ REM Pindah ke direktori tempat skrip ini berada
 cd /d %~dp0
 cd sistem
 
+REM ####################################################################
+REM ### PENTING: Menambahkan path Node.js portabel ke PATH sementara ###
+set "PATH=%CD%\..\bin\nodejs;%PATH%"
+REM ####################################################################
+
 REM ### LANGKAH 2: MULAI LARAVEL ARTISAN SERVER ###
 echo [2/3] Memulai Laravel Server di http://%HOST_IP%:%LARAVEL_PORT%
 START "Laravel Backend" ..\bin\php\php.exe artisan serve --host=%HOST_IP% --port=%LARAVEL_PORT%
 
 REM ### LANGKAH 3: MULAI VITE DEV SERVER ###
 echo [3/3] Memulai Vite Frontend Server...
-START "Vite Frontend" call ..\bin\nodejs\npm.cmd run dev -- --host
+rem Sekarang kita bisa memanggil npm secara langsung karena path sudah diatur
+START "Vite Frontend" call npm run dev -- --host
 
 cd ..
 
